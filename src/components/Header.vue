96f241c2f6f8985745d62e28941ccfa51902acaa
Alvaro <alvaro.rivas.p@gmail.com>
2021-11-17 00:45:11 -0300

    desafio terminado

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <h1 :style="{color: this.color}">{{titulo}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        titulo: String, color: String
+    }
+}
+</script>
